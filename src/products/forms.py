200251fd28121de4d26c0af45f7bb417048d9076
ongorio <ongorio5@gmail.com>
2023-06-17 20:12:04 -0600

    product detail style

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,9 @@ from django import forms
 from products.models import Product
 
 class ProductRegisterForm(forms.ModelForm):
+    price = forms.DecimalField(min_value=0)
+    quantity = forms.IntegerField(min_value=0)
+
     class Meta:
         model = Product
         fields = (
